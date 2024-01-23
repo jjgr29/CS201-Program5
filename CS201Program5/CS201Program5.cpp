3fc0f27077757ec4166592fe960af51d9815d6c4
jjgr29 <100229180+jjgr29@users.noreply.github.com>
2024-01-23 11:05:54 -0600

    Create Template

---
+++ CS201Program5/CS201Program5.cpp
@@ -0,0 +1,11 @@
+// CS201Program5.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!\n";
+}
+ 
